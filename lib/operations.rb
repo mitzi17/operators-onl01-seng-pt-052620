1b106123ae911add144266e4bc00c1d375b2f917
Mitzi Estrada <m_estrada17@live.com>
2020-05-31 23:06:45 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
-  if speed > 40
-    return true
-  elsif speed < 60
-    return true
+  if speed < 40
+    true
+  elsif speed > 60
+    true
   else 
-    return false
+    false
   end
 end
 
